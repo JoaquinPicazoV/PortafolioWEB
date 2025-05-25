30597b88e0a1942f1ac892c9a949b6aa09aeb763
JoaquinPicazoV <joaquinpicazovelasco@gmail.com>
2025-05-25 17:10:59 -0400

    mejorado

--- src/components/netandcyb.jsx
+++ src/components/netandcyb.jsx
@@ -4,12 +4,30 @@ import PACKET from '../assets/logos/packet.png';
 function NETANDCYB() {
   const projects = [
     {
-      title: "Conexión LAN-WAN",
-      subtitle: "Redes LAN interconectadas por WAN entre ciudades (sin medidas de seguridad).",
+      title: "Configuración de Switch con Seguridad Básica",
+      subtitle: "Configuración de VLANs y seguridad en puertos para limitar conexiones físicas y proteger la red contra dispositivos no autorizados y ataques en capa 2.",
       languages: [{ name: "Packet Tracer", logo: PACKET }]
-    }
-    
-
+    },
+    {
+      title: "VLAN básica",
+      subtitle: "Segmentación de red mediante VLANs para aislar tráfico entre departamentos, reduciendo riesgos de acceso no autorizado y ataques laterales.",
+      languages: [{ name: "Packet Tracer", logo: PACKET }]
+    },
+    {
+      title: "LAN con servidor DHCP",
+      subtitle: "Red LAN simple con servidor DHCP. Sin embargo, la configuración actual permite que un atacante podría configurar la IP y máscara manualmente, permitiendo comunicarse con los dispositivos de la red.",
+      languages: [{ name: "Packet Tracer", logo: PACKET }]
+    },
+    {
+      title: "Conexión LAN-WAN",
+      subtitle: "Redes LAN interconectadas por WAN entre ciudades. Todos los dispoistivos tienen comunicación entre si y sin medidas de seguridad.",
+      languages: [{ name: "Packet Tracer", logo: PACKET }]
+    },
+    {
+      title: "Topología de Red LAN Simple",
+      subtitle: "Red LAN simple. Dos dispositivos en la misma red, con comunicación entre si. Evidencia la necesidad de segmentación, VLANs y ACLs.",
+      languages: [{ name: "Packet Tracer", logo: PACKET }]
+    },
   ];
 
   return (
